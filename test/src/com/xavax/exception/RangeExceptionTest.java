399a662541133acb307f942aff9c90f8f7620c19
Phil Harbison <alvitar@xavax.com>
2015-06-16 03:22:15 -0400

    Added test for RangeException.

---
+++ test/src/com/xavax/exception/RangeExceptionTest.java
@@ -0,0 +1,41 @@
+package com.xavax.exception;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Test cases for the RangeException class.
+ */
+public class RangeExceptionTest {
+  private final static int MIN = 1;
+  private final static int MAX = 10;
+  private final static int VALUE = 11;
+
+  private final RangeException exception =
+      new RangeException(MIN, MAX, VALUE);
+
+  /**
+   * Test the getAttempted method.
+   */
+  @Test
+  public void getAttempted() {
+    assertEquals(exception.getAttempted(), VALUE);
+  }
+
+  /**
+   * Test the getMaximum method.
+   */
+  @Test
+  public void getMaximum() {
+    assertEquals(exception.getMaximum(), MAX);
+  }
+
+  /**
+   * Test the getMinimum method.
+   */
+  @Test
+  public void getMinimum() {
+    assertEquals(exception.getMinimum(), MIN);
+  }
+}
